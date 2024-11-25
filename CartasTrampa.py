a5255563b29ed15cac273be52b7d13054fe740f5
Roalanvi <82995445+Roalanvi@users.noreply.github.com>
2024-11-24 23:55:15 -0500

    Delete CartasTrampa.py

--- CartasTrampa.py
+++
@@ -1,8 +0,0 @@
-from Cartas import Carta
-from CartasMonstruos import Monstruo
-class Trampa(Carta):
-    def __init__(self, nombre, descripcion, atributo):
-        super().__init__(self, nombre, descripcion)
-        self.atributo = atributo
-    def activar(self, monstruo_atacante: Monstruo):
-        pass
